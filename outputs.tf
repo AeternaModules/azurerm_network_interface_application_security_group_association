7d76b0bd7a12d969202a0899482620561b9fa455
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:03:44 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "network_interface_application_security_group_associations_id" {
+  description = "Map of id values across all network_interface_application_security_group_associations, keyed the same as var.network_interface_application_security_group_associations"
+  value       = { for k, v in azurerm_network_interface_application_security_group_association.network_interface_application_security_group_associations : k => v.id }
+}
 output "network_interface_application_security_group_associations_application_security_group_id" {
   description = "Map of application_security_group_id values across all network_interface_application_security_group_associations, keyed the same as var.network_interface_application_security_group_associations"
   value       = { for k, v in azurerm_network_interface_application_security_group_association.network_interface_application_security_group_associations : k => v.application_security_group_id }
