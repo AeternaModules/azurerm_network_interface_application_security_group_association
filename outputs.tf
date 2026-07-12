bc865cbb57059a9ec17c4a7ef5641430813775c6
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:17 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "network_interface_application_security_group_associations" {
-  description = "All network_interface_application_security_group_association resources"
-  value       = azurerm_network_interface_application_security_group_association.network_interface_application_security_group_associations
-}
 output "network_interface_application_security_group_associations_application_security_group_id" {
-  description = "List of application_security_group_id values across all network_interface_application_security_group_associations"
-  value       = [for k, v in azurerm_network_interface_application_security_group_association.network_interface_application_security_group_associations : v.application_security_group_id]
+  description = "Map of application_security_group_id values across all network_interface_application_security_group_associations, keyed the same as var.network_interface_application_security_group_associations"
+  value       = { for k, v in azurerm_network_interface_application_security_group_association.network_interface_application_security_group_associations : k => v.application_security_group_id }
 }
 output "network_interface_application_security_group_associations_network_interface_id" {
-  description = "List of network_interface_id values across all network_interface_application_security_group_associations"
-  value       = [for k, v in azurerm_network_interface_application_security_group_association.network_interface_application_security_group_associations : v.network_interface_id]
+  description = "Map of network_interface_id values across all network_interface_application_security_group_associations, keyed the same as var.network_interface_application_security_group_associations"
+  value       = { for k, v in azurerm_network_interface_application_security_group_association.network_interface_application_security_group_associations : k => v.network_interface_id }
 }
 
